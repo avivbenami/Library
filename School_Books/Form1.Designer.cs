be8687e8ea5917af58d55c526bac2ce3fe3f4e9b
aviv <avivbenami@gmail.com>
2020-01-16 01:43:33 +0200

    Add project files.

---
+++ School_Books/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace School_Books
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(220, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.comboBox1.Size = new System.Drawing.Size(185, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(421, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(71, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "כיתה";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(220, 77);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.comboBox2.Size = new System.Drawing.Size(185, 21);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(421, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(71, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "שם תלמיד";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridView1.Size = new System.Drawing.Size(529, 205);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 335);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ספר חדש";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(58, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(552, 366);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ספרייה";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ School_Books/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+//using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+using System.Data.OleDb;
+using DataTable = System.Data.DataTable;
+
+namespace School_Books
+{
+    public partial class Form1 : Form
+    {
+        readonly string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=books.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=0' ";
+        readonly Dictionary<int, string> column =
+            new Dictionary<int, string>
+            {
+                { 1, "Book" },
+                { 2, "BorrowDate" },
+                { 3, "ReturnDate" }
+            };
+        public string Sheet { get { return comboBox1.SelectedItem.ToString(); } }
+        public string Student { get { return comboBox2.SelectedItem.ToString(); } }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private string[] GetSheets()
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    DataTable dt = new DataTable();
+                    dt = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    string[] excelSheets = new String[dt.Rows.Count];
+                    int i = 0;
+                    foreach (DataRow dataRow in dt.Rows)
+                    {
+                        excelSheets[i] = dataRow["TABLE_NAME"].ToString().Trim('\'', '$');
+                        i++;
+                    }
+                    return excelSheets;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return new string[1];
+
+                }
+            }
+        }
+        private string[] GetStudents(string sheet)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string cmd = "SELECT DISTINCT Student FROM [" + sheet + "$]";
+                    Debug.WriteLine(cmd);
+                    OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(cmd, connection);
+                    DataTable dt = new DataTable();
+                    myDataAdapter.Fill(dt);
+                    string[] students = new String[dt.Rows.Count];
+                    int i = 0;
+                    foreach (DataRow dataRow in dt.Rows)
+                    {
+                        students[i] = dataRow[0].ToString();
+                        i++;
+                    }
+                    myDataAdapter.Dispose();
+                    return students;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return new string[1];
+                }
+            }
+        }
+        private string GetID(string sheet)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string cmd = "SELECT MAX(ID) FROM [" + sheet + "$];";
+                    Debug.WriteLine(cmd);
+                    OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(cmd, connection);
+                    DataTable dt = new DataTable();
+                    myDataAdapter.Fill(dt);
+
+                    myDataAdapter.Dispose();
+                    return dt.Rows[0][0].ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return "";
+                }
+            }
+        }
+        private void UpdateGrid(string sheet, string student)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string cmd = "SELECT ID, Book, BorrowDate, ReturnDate FROM [" + sheet + "$] WHERE Student=\"" + student + "\"";
+                    Debug.WriteLine(cmd);
+                    OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(cmd, connection);
+                    DataTable dt = new DataTable();
+                    myDataAdapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Columns[0].Visible = false;
+                    dataGridView1.Columns["Book"].Width = 120;
+                    dataGridView1.Columns["Book"].HeaderText = "שם הספר";
+                    dataGridView1.Columns["BorrowDate"].Width = 120;
+                    dataGridView1.Columns["BorrowDate"].HeaderText = "תאריך השאלה";
+                    dataGridView1.Columns["ReturnDate"].Width = 120;
+                    dataGridView1.Columns["ReturnDate"].HeaderText = "תאריך החזרה";
+                    myDataAdapter.Dispose();
+                    return;
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return;
+                }
+            }
+        }
+        private void UpdateCell(string sheet, DataGridViewCellEventArgs e)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    if (e.RowIndex == -1) { return; }
+                    connection.Open();
+                    string id = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                    string newValue = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString().Replace("'", "''");
+                    string cmd = "UPDATE [" + sheet + "$] SET " + column[e.ColumnIndex] + " = '" + newValue + "' WHERE ID=" + id;
+                    Debug.WriteLine(cmd);
+                    OleDbCommand olecmd = new OleDbCommand(cmd, connection);
+                    olecmd.ExecuteNonQuery();
+                    return;
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return;
+                }
+            }
+        }
+        private void AddRow(string sheet, string student)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (OleDbDataAdapter da = new OleDbDataAdapter())
+                    using (OleDbCommandBuilder bld = new OleDbCommandBuilder(da))
+                    {
+                        bld.QuotePrefix = "[";  // these are
+                        bld.QuoteSuffix = "]";  //   important!
+
+                        da.SelectCommand = new OleDbCommand(
+                                "SELECT [ID], [Student], [Book], [BorrowDate], [ReturnDate] " +
+                                "FROM [" + sheet + "$] " +
+                                "WHERE False",
+                                connection);
+                        Debug.WriteLine(da.SelectCommand);
+                        using (DataTable dt = new System.Data.DataTable("Test"))
+                        {
+                            // create an empty DataTable with the correct structure
+
+                            da.Fill(dt);
+                            DataRow dr = dt.NewRow();
+
+                            dr["ID"] = (Int32.Parse(GetID(sheet)) + 1).ToString();
+                            dr["Student"] = student;
+                            dr["Book"] = null;
+                            dr["BorrowDate"] = null;
+                            dr["ReturnDate"] = null;
+                            dt.Rows.Add(dr);
+
+                            da.Update(dt);  // write new row back to database
+                        }
+                    }
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return;
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ToolTip toolTip1 = new ToolTip
+            //{
+            //    AutoPopDelay = 5000,
+            //    InitialDelay = 1000,
+            //    ReshowDelay = 500,
+            //    ShowAlways = true
+            //};
+            //toolTip1.SetToolTip(this.button1, "My button1");
+
+            string[] excelSheets = GetSheets();
+            comboBox1.Items.AddRange(excelSheets);
+            comboBox1.SelectedIndex = 0;
+        }
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = "";
+            dataGridView1.DataSource = null;
+            comboBox2.Items.Clear();
+            
+            string[] students = GetStudents(Sheet);
+            comboBox2.Items.AddRange(students);
+
+        }
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateGrid(Sheet, Student);
+        }
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            UpdateCell(Sheet, e);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text.Length == 0)
+            {
+                return;
+            }
+            AddRow(Sheet, Student);
+            UpdateGrid(Sheet, Student);
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            ComboBox1_SelectedIndexChanged(null, null);
+        }
+    }
+}
