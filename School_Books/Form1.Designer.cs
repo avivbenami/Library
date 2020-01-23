e62a43632efd8f410c7c4238bb02ad1b9af87397
aviv <avivbenami@gmail.com>
2020-01-23 19:04:03 +0200

    Comboox now DropDownList new ClearForm func added check if combobox is null

--- School_Books/Form1.Designer.cs
+++ School_Books/Form1.Designer.cs
@@ -42,6 +42,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(220, 40);
             this.comboBox1.Name = "comboBox1";
@@ -63,6 +64,7 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(220, 77);
             this.comboBox2.Name = "comboBox2";
